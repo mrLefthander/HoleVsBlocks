1dadafcd4447e1861b7b9a921a48b14d08cee502
mrLefthander <lefthander89@gmail.com>
2021-10-03 16:46:24 +0300

    IsMoving logic update

--- Assets/Scripts/GameStateSO.cs
+++ Assets/Scripts/GameStateSO.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Game State", menuName = "Game/Game State")]
 public class GameStateSO : ScriptableObject
 {
-  public bool IsMoving = false;
+  public bool IsMoving => Input.GetMouseButton(0);
   public bool IsGameOver = false;
 }
