56b2ee2562d808193042d883217ffed871c94c75
mrLefthander <lefthander89@gmail.com>
2021-10-19 12:23:48 +0300

    Added mobile controls

--- Assets/Scripts/ScriptableObjects/GameStateSO.cs
+++ Assets/Scripts/ScriptableObjects/GameStateSO.cs
@@ -3,7 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Game State", menuName = "Game/Game State")]
 public class GameStateSO : ScriptableObject
 {
+#if UNITY_EDITOR
   public bool IsMoving => Input.GetMouseButton(0);
+#else
+  public bool IsMoving => Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved;
+#endif
+
   public bool IsGameOver = false;
 
   private void OnEnable()
